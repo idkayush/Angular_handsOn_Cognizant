7aa488b7ce8416aea5dd5b2c79a11d37d2aff08f
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 13:15:50 +0530

    LoggingExample.java

--- Ayush_Arvind/week_4/07_SLF4J_Logging_Exercises/src/main/java/LoggingExample.java
+++ Ayush_Arvind/week_4/07_SLF4J_Logging_Exercises/src/main/java/LoggingExample.java
@@ -1,1 +1,15 @@
-package com.ayush.logging; import org.slf4j.Logger; import org.slf4j.LoggerFactory; public class LoggingExample { private static final Logger logger=LoggerFactory.getLogger(LoggingExample.class); public static void main(String[] args){ logger.error("This is an error message"); logger.warn("This is a warning message"); String user="Ayush"; int attempts=3; logger.info("User {} logged in after {} attempts", user, attempts); logger.debug("Debug log written to console and app.log"); }}
+package com.ayush.logging; 
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory; 
+public class LoggingExample
+  {
+    private static final Logger logger=LoggerFactory.getLogger(LoggingExample.class);
+    public static void main(String[] args)
+    {
+      logger.error("This is an error message"); 
+      logger.warn("This is a warning message"); 
+      String user="Ayush"; int attempts=3; 
+      logger.info("User {} logged in after {} attempts", user, attempts); 
+      logger.debug("Debug log written to console and app.log"); 
+    }
+  }
