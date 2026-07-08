2edeecf643db86dfe99aee648cd86cf97b18201e
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 13:14:29 +0530

    UserService.java

--- Ayush_Arvind/week_4/06_Mockito_Mock_Dependencies/src/main/java/UserService.java
+++ Ayush_Arvind/week_4/06_Mockito_Mock_Dependencies/src/main/java/UserService.java
@@ -1,1 +1,19 @@
-package com.ayush.springtest; import org.springframework.stereotype.Service; import java.util.*; @Service public class UserService { private final UserRepository userRepository; public UserService(UserRepository userRepository){this.userRepository=userRepository;} public User getUserById(Long id){ return userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("User not found")); } public User saveUser(User user){ return userRepository.save(user); } }
+package com.ayush.springtest;
+import org.springframework.stereotype.Service; 
+import java.util.*; 
+@Service public class UserService 
+{
+  private final UserRepository userRepository;
+  public UserService(UserRepository userRepository)
+  {
+    this.userRepository=userRepository;
+  }
+  public User getUserById(Long id)
+  {
+    return userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("User not found")); 
+  }
+  public User saveUser(User user)
+  {
+    return userRepository.save(user); 
+  }
+}
