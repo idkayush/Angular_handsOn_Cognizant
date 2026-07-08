f5b653ea688bbd3a9ba643333ecd5654d08edd36
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 12:55:25 +0530

    FileService.java

--- Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/FileService.java
+++ Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/FileService.java
@@ -1,1 +1,16 @@
-package com.ayush.advancedmock; public class FileService { private final FileReader reader; private final FileWriter writer; public FileService(FileReader reader, FileWriter writer){this.reader=reader;this.writer=writer;} public String processFile(){ String result="Processed "+reader.read(); writer.write(result); return result; }}
+package com.ayush.advancedmock; 
+public class FileService
+  { 
+    private final FileReader reader; 
+    private final FileWriter writer; 
+    public FileService(FileReader reader, FileWriter writer)
+    {
+      this.reader=reader;this.writer=writer;
+    }
+    public String processFile()
+    { 
+      String result="Processed "+reader.read();
+      writer.write(result); 
+      return result; 
+    }
+  }
