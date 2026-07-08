b868e380100c3775002571bdc54cb011788fbda0
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 12:56:56 +0530

    NetworkService.java

--- Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/NetworkService.java
+++ Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/NetworkService.java
@@ -1,1 +1,12 @@
-package com.ayush.advancedmock; public class NetworkService { private final NetworkClient client; public NetworkService(NetworkClient client){this.client=client;} public String connectToServer(){return "Connected to "+client.connect();}}
+package com.ayush.advancedmock; 
+public class NetworkService 
+{
+  private final NetworkClient client; 
+  public NetworkService(NetworkClient client)
+  {
+    this.client=client;
+  } public String connectToServer()
+  {
+    return "Connected to "+client.connect();
+  }
+}
