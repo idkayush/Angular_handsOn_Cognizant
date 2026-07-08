96ea32afa3811b58289345b506225630943b3f1a
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 13:05:20 +0530

    UserRepository.java

--- Ayush_Arvind/week_4/05_JUnit_Spring_Test_Exercises/src/main/java/UserRepository.java
+++ Ayush_Arvind/week_4/05_JUnit_Spring_Test_Exercises/src/main/java/UserRepository.java
@@ -1,1 +1,7 @@
-package com.ayush.springtest; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface UserRepository extends JpaRepository<User,Long>{ List<User> findByName(String name); }
+package com.ayush.springtest;
+import org.springframework.data.jpa.repository.JpaRepository; 
+import java.util.*;
+public interface UserRepository extends JpaRepository<User,Long>
+{ 
+  List<User> findByName(String name);
+}
