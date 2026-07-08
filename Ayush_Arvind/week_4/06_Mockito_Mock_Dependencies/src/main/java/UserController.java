168e2233dd69096e9106b154190b6bf18e6375ae
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 13:12:47 +0530

    UserController.java

--- Ayush_Arvind/week_4/06_Mockito_Mock_Dependencies/src/main/java/UserController.java
+++ Ayush_Arvind/week_4/06_Mockito_Mock_Dependencies/src/main/java/UserController.java
@@ -1,1 +1,22 @@
-package com.ayush.springtest; import org.springframework.http.ResponseEntity; import org.springframework.web.bind.annotation.*; @RestController @RequestMapping("/users") public class UserController { private final UserService userService; public UserController(UserService userService){this.userService=userService;} @GetMapping("/{id}") public ResponseEntity<User> getUser(@PathVariable Long id){ return ResponseEntity.ok(userService.getUserById(id)); } @PostMapping public ResponseEntity<User> createUser(@RequestBody User user){ return ResponseEntity.ok(userService.saveUser(user)); } }
+package com.ayush.springtest; 
+import org.springframework.http.ResponseEntity; 
+import org.springframework.web.bind.annotation.*; 
+@RestController @RequestMapping("/users")
+  public class UserController 
+{
+  private final UserService userService; 
+  public UserController(UserService userService)
+  {
+    this.userService=userService;
+  }
+  @GetMapping("/{id}")
+  public ResponseEntity<User> getUser(@PathVariable Long id)
+  {
+    return ResponseEntity.ok(userService.getUserById(id));
+  } 
+  @PostMapping 
+  public ResponseEntity<User> createUser(@RequestBody User user)
+  {
+    return ResponseEntity.ok(userService.saveUser(user)); 
+  }
+}
