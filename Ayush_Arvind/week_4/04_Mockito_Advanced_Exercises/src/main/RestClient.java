d975b68270421fe749e18f8a993630d7df844f72
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 12:58:15 +0530

    RestClient.java

--- Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/RestClient.java
+++ Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/RestClient.java
@@ -1,1 +1,5 @@
-package com.ayush.advancedmock; public interface RestClient { String getResponse(); }
+package com.ayush.advancedmock; 
+public interface RestClient
+  {
+    String getResponse(); 
+  }
