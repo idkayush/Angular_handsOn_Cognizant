a52d03be4f5f59a125d031545399e379a0607bd4
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-08 12:59:05 +0530

    Service.java

--- Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/Service.java
+++ Ayush_Arvind/week_4/04_Mockito_Advanced_Exercises/src/main/Service.java
@@ -1,1 +1,13 @@
-package com.ayush.advancedmock; public class Service { private final Repository repository; public Service(Repository repository){this.repository=repository;} public String processData(){return "Processed "+repository.getData();}}
+package com.ayush.advancedmock; 
+public class Service
+  {
+    private final Repository repository;
+    public Service(Repository repository)
+    {
+      this.repository=repository;
+    }
+    public String processData()
+    {
+      return "Processed "+repository.getData();
+    }
+  }
