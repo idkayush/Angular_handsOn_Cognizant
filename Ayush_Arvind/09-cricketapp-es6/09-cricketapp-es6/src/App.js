e6de00aebafb396c5cb3777822609722b37cf983
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-22 11:51:28 +0530

    Delete Ayush_Arvind/09-cricketapp-es6/09-cricketapp-es6 directory

--- Ayush_Arvind/09-cricketapp-es6/09-cricketapp-es6/src/App.js
+++
@@ -1,9 +0,0 @@
-import ListOfPlayers from "./ListOfPlayers";
-import IndianPlayers from "./IndianPlayers";
-
-function App() {
-    const flag = true;
-    return flag ? <ListOfPlayers /> : <IndianPlayers />;
-}
-
-export default App;
