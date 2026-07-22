cbc86b1137ba4526146eddf3e2159510175d0091
Ayush Arvind <ayusharvind2k18@gmail.com>
2026-07-22 11:55:22 +0530

    Add files via upload

---
+++ Ayush_Arvind/week-7/06-TrainersApp-router/06-TrainersApp-router/src/TrainersMock.js
@@ -0,0 +1,22 @@
+import Trainer from "./trainer";
+
+const trainers = [
+    new Trainer(
+        1,
+        "Syed Khaleelullah",
+        "khaleelullah@cognizant.com",
+        "97676516962",
+        ".NET",
+        ["C#", "SQL Server", "React", ".NET Core"]
+    ),
+    new Trainer(
+        2,
+        "John Mathew",
+        "john@cognizant.com",
+        "9876543210",
+        "Java",
+        ["Java", "Spring Boot", "Microservices"]
+    )
+];
+
+export default trainers;
